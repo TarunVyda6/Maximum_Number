f3001fa0cfd9c84ab3a4eb605f869fbedfbb07a3
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-27 10:03:45 +0530

    Added : Feature of finding maximum of three float numbers and their test cases

--- MaximumNumber/src/main/java/org/maximumnumber/MaximumNumber.java
+++ MaximumNumber/src/main/java/org/maximumnumber/MaximumNumber.java
@@ -3,13 +3,25 @@ package org.maximumnumber;
 public class MaximumNumber {
 
 	// uc1
-	public static Integer maximumNumber(Integer a, Integer b, Integer c) {
-		Integer maxNumber = a;
-		if (b.compareTo(maxNumber) > 0) {
-			maxNumber = b;
+	public static Integer maximumNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+		Integer maxNumber = firstNumber;
+		if (secondNumber.compareTo(maxNumber) > 0) {
+			maxNumber = secondNumber;
 		}
-		if (c.compareTo(maxNumber) > 0) {
-			maxNumber = c;
+		if (thirdNumber.compareTo(maxNumber) > 0) {
+			maxNumber = thirdNumber;
+		}
+		return maxNumber;
+	}
+
+	// uc2
+	public static Float maximumNumber(Float firstNumber, Float secondNumber, Float thirdNumber) {
+		Float maxNumber = firstNumber;
+		if (secondNumber.compareTo(maxNumber) > 0) {
+			maxNumber = secondNumber;
+		}
+		if (thirdNumber.compareTo(maxNumber) > 0) {
+			maxNumber = thirdNumber;
 		}
 		return maxNumber;
 	}
--- MaximumNumber/src/test/java/org/maximumnumber/MaximumNumberTest.java
+++ MaximumNumber/src/test/java/org/maximumnumber/MaximumNumberTest.java
@@ -4,22 +4,39 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumNumberTest {
-
+	// Tc1.1
 	@Test
-	public void givenMaximumNumberAtFirst_WhenAnalyse_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maximumNumber(25, 10, 15);
-		Assert.assertEquals((Integer) 25, result);
+	public void givenMaximumIntegerNumberAtFirst_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Integer) 25, MaximumNumber.maximumNumber(25, 10, 15));
 	}
 
+	// Tc1.2
 	@Test
-	public void givenMaximumNumberAtSecond_WhenAnalyse_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maximumNumber(22, 90, 15);
-		Assert.assertEquals((Integer) 90, result);
+	public void givenMaximumIntegerNumberAtSecond_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Integer) 90, MaximumNumber.maximumNumber(22, 90, 15));
 	}
 
+	// Tc1.3
 	@Test
-	public void givenMaximumNumberAtThird_WhenAnalyse_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maximumNumber(16, 22, 65);
-		Assert.assertEquals((Integer) 65, result);
+	public void givenMaximumIntegerNumberAtThird_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Integer) 65, MaximumNumber.maximumNumber(16, 22, 65));
+	}
+
+	// Tc2.1
+	@Test
+	public void givenMaximumFloatNumberAtFirst_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Float) 25.6f, MaximumNumber.maximumNumber(25.6f, 10.4f, 15.6f));
+	}
+
+	// Tc2.2
+	@Test
+	public void givenMaximumFloatNumberAtSecond_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Float) 90.1f, MaximumNumber.maximumNumber(22.9f, 90.1f, 15.4f));
+	}
+
+	// Tc2.3
+	@Test
+	public void givenMaximumFloatNumberAtThird_WhenAnalyse_ShouldReturnSameNumber() {
+		Assert.assertEquals((Float) 65.7f, MaximumNumber.maximumNumber(16.6f, 22.3f, 65.7f));
 	}
 }
