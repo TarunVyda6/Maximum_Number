33eee4ae029819438383e9acab85f48e6ce7308b
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-10-27 09:47:28 +0530

    Added : Feature to find maximum of three integers and their test cases

--- MaximumNumber/src/main/java/org/maximumnumber/MaximumNumber.java
+++ MaximumNumber/src/main/java/org/maximumnumber/MaximumNumber.java
@@ -1,5 +1,17 @@
 package org.maximumnumber;
 
 public class MaximumNumber {
-	
+
+	// uc1
+	public static Integer maximumNumber(Integer a, Integer b, Integer c) {
+		Integer maxNumber = a;
+		if (b.compareTo(maxNumber) > 0) {
+			maxNumber = b;
+		}
+		if (c.compareTo(maxNumber) > 0) {
+			maxNumber = c;
+		}
+		return maxNumber;
+	}
+
 }
--- MaximumNumber/src/test/java/org/maximumnumber/MaximumNumberTest.java
+++ MaximumNumber/src/test/java/org/maximumnumber/MaximumNumberTest.java
@@ -4,10 +4,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MaximumNumberTest {
-	
+
+	@Test
+	public void givenMaximumNumberAtFirst_WhenAnalyse_ShouldReturnSameNumber() {
+		Integer result = MaximumNumber.maximumNumber(25, 10, 15);
+		Assert.assertEquals((Integer) 25, result);
+	}
+
+	@Test
+	public void givenMaximumNumberAtSecond_WhenAnalyse_ShouldReturnSameNumber() {
+		Integer result = MaximumNumber.maximumNumber(22, 90, 15);
+		Assert.assertEquals((Integer) 90, result);
+	}
+
+	@Test
+	public void givenMaximumNumberAtThird_WhenAnalyse_ShouldReturnSameNumber() {
+		Integer result = MaximumNumber.maximumNumber(16, 22, 65);
+		Assert.assertEquals((Integer) 65, result);
+	}
 }
-
-
-
-
-
